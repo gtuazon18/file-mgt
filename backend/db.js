d4e47268cc2df59464436aca80b5d88bc99f4a91
gtuazon18 <tuazon548@gmail.com>
2024-11-25 22:27:29 +0800

    add mysql

--- backend/db.js
+++ backend/db.js
@@ -1,13 +1,12 @@
 const mysql = require('mysql2/promise');
 
-// Create a promise-based connection to the MySQL database
 const db = mysql.createPool({
   host: '127.0.0.1',
   user: 'root',
-  password: '', // Update your password if needed
+  password: '', 
   database: 'file_management',
   waitForConnections: true,
-  connectionLimit: 10,  // You can adjust this limit if necessary
+  connectionLimit: 10,  
   queueLimit: 0
 });
 
