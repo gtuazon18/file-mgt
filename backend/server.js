ece4fdbffff8ec142b99aebdb4bf11a867116cd4
gtuazon18 <tuazon548@gmail.com>
2024-11-26 21:02:09 +0800

    add cors

--- backend/server.js
+++ backend/server.js
@@ -26,8 +26,15 @@ const port = process.env.PORT || 5000;
 // Middleware
 app.use(cors());
 app.use(express.json());
+const allowedOrigins = ['https://file-mgt.vercel.app/'];
 
-// Multer storage configuration
+app.use(cors({
+  origin: allowedOrigins, 
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true, 
+}));
+
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, uploadDir);
